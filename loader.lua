598f7fa62cc3101826d81d55289f9f75e5036291
ImMejor35 <104869883+ImMejor35@users.noreply.github.com>
2023-08-09 06:27:51 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,7 @@
+local Build = "58"
+local CurrentBuild = game:GetService("ReplicatedStorage").Config:GetAttribute("BuildVersion")
+if Build ~= CurrentBuild then
+  warn("Build mismatch bugs and errors may be caused due to this!")
+end
 local Branch = ...
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ImMejor35/Flood-GUI/"..Branch.."/Flood%20GUI%20v3"))(Branch)
