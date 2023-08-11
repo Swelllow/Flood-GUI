71dd89fafbfa4d6eb6997c81879e69b8a1f0a7c4
ImMejor35 <garyjerrolds@outlook.com>
2023-08-11 04:48:37 -0700

    brrha

--- loader.lua
+++ loader.lua
@@ -4,4 +4,11 @@ if Build ~= CurrentBuild then
   warn("Build mismatch bugs and errors may be caused due to this!")
 end
 local Branch = ...
+
+loadfile = (isfile("Flood-GUI") and loadfile) or function(path)
+  if path:match("Flood-GUI") then
+    local realpath = game:GetService("HttpService"):UrlEncode(path:sub(9, #path))
+    return loadstring(game:HttpGet("https://raw.githubusercontent.com/ImMejor35/Flood-GUI/"..Branch.."/"..realpath))
+  end
+end
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ImMejor35/Flood-GUI/"..Branch.."/Flood%20GUI%20v3"))(Branch)
