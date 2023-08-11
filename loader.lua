0569deedeb811ce94d470e2a403f666cfe5e3884
ImMejor35 <garyjerrolds@outlook.com>
2023-08-11 05:36:18 -0700

    turns out it was a dead body

--- loader.lua
+++ loader.lua
@@ -1,7 +1,8 @@
 local LatestBuild = 58
 local CurrentBuild = tonumber(game:GetService("ReplicatedStorage").Config:GetAttribute("BuildVersion"))
-if LatestBuild > CurrentBuild then
+if LatestBuild < CurrentBuild then
   warn("Build mismatch bugs and errors may be caused due to this!")
+  warn("Current Build:", CurrentBuild)
 end
 local Branch = ...
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ImMejor35/Flood-GUI/"..Branch.."/Flood%20GUI%20v3"))(Branch)
+loadfile("Flood-GUI/Flood GUI v3")(Branch)
