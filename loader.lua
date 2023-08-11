790f8f4197bd21680417555da3f1f7f85650407e
ImMejor35 <104869883+ImMejor35@users.noreply.github.com>
2023-08-11 15:17:26 -0700

    Add files via upload

---
+++ loader.lua
@@ -0,0 +1,31 @@
+local LatestBuild = 58
+local CurrentBuild = tonumber(game:GetService("ReplicatedStorage").Config:GetAttribute("BuildVersion"))
+if LatestBuild < CurrentBuild then
+  warn("Build mismatch bugs and errors may be caused due to this!")
+  warn("Current Build:", CurrentBuild)
+end
+
+local Branch = "systest"
+local FloodGUIRepo = "https://raw.githubusercontent.com/ImMejor35/Flood-GUI/"..Branch
+if not isfolder("Flood-GUI") then
+	makefolder("Flood-GUI")
+end
+if not isfolder("Flood-GUI/TAS") then
+    makefolder("Flood-GUI/TAS")
+end
+
+local function import(webpath)
+    local filepath = "Flood-GUI"..webpath
+    local fullurl = (FloodGUIRepo..webpath):gsub(" ", "%%20")
+    if not isfile(filepath) then
+        writefile(filepath, game:HttpGet(fullurl))
+        return true
+    end
+    return false
+end
+
+local startimport = tick()
+if import("/Flood GUI v3") and import("/TAS/TAS Editor") and import("/TAS/TAS Player") and import("/TAS/TAS Record Voiz") then
+    print("Imported Flood GUI in "..tostring(tick() - startimport))
+end
+loadfile("Flood-GUI/Flood GUI v3")()
